5d746c02ff350e9f5378285d2fa14cfdec8b6b98
hanyauntuksubmit <113534457+hanyauntuksubmit@users.noreply.github.com>
2022-09-22 22:47:15 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,38 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+        <>
+            <List  className='contact-list' sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {data.map((todo, index) => {
+                return (
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <img src={todo.photo} alt="Logo" />
+                            </Avatar>
+                        </ListItemAvatar>
+                    <ListItemText primary={todo.name} secondary={
+                        <React.Fragment>
+                            {todo.phone} <br/> {todo.email}
+                        </React.Fragment>
+                    } />
+                </ListItem>
+                );
+            })}
+            </List>
+        </>
+    );
 };
 
 export default Contact;
