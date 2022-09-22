f939a5ad8ccc91468b5caa99378b91915cf50f49
hanyauntuksubmit <113534457+hanyauntuksubmit@users.noreply.github.com>
2022-09-22 23:09:54 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,28 +12,37 @@ import Avatar from '@mui/material/Avatar';
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (
-        <>
-            <List  className='contact-list' sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {data.map((todo, index) => {
-                return (
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <img src={todo.photo} alt="Logo" />
-                            </Avatar>
-                        </ListItemAvatar>
-                    <ListItemText primary={todo.name} secondary={
-                        <React.Fragment>
-                            {todo.phone} <br/> {todo.email}
-                        </React.Fragment>
-                    } />
-                </ListItem>
-                );
-            })}
-            </List>
-        </>
-    );
+    if (Array.isArray(data)) {
+        return (
+            <>
+                <List  className='contact-list' sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {data.map((todo, index) => {
+                        return (
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <img src={todo.photo} alt="Logo" />
+                                    </Avatar>
+                                </ListItemAvatar>
+                            <ListItemText primary={todo.name} secondary={
+                                <React.Fragment>
+                                    {todo.phone} <br/> {todo.email}
+                                </React.Fragment>
+                            } />
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </>
+        );
+    } else {
+        return (
+            <>
+                Monica 087577588432 devi@email.com
+                <img src="http://placekitten.com/600" alt="Logo" />
+            </>
+        );
+    }
 };
 
 export default Contact;
