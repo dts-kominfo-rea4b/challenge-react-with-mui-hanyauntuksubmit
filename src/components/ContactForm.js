f1c1c1992608ca91bf0a8d5aaeac6edf45d9665e
hanyauntuksubmit <113534457+hanyauntuksubmit@users.noreply.github.com>
2022-09-22 22:47:38 +0700

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,12 +3,32 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+
+const ContactForm = ({onClickHandler}) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [todo, setTodo] = useState("");
 
     return (
-        <> </>
+        <> 
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <TextField required id="f-name" label="Name" variant="filled" onChange={(e) => {setTodo(e.target.value)}} />
+                    <TextField required id="f-phone" label="Phone" variant="filled" onChange={(e) => {setTodo(e.target.value)}} />
+                    <TextField required id="f-email" label="Email" variant="filled" onChange={(e) => {setTodo(e.target.value)}} />
+                    <TextField required id="f-photo" label="Photo URL" variant="filled" onChange={(e) => {setTodo(e.target.value)}} />
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={onClickHandler}>ADD NEW</Button>
+                </CardActions>
+            </Card>
+        </>
     );
 }
 
